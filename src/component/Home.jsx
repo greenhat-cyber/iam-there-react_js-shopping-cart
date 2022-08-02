3957dc0f53ba4ef3ca05537f1026b471793a18ab
Login-Registration-form-python <vishalkunnam124@gmail.com>
2022-08-02 15:53:43 +0530

    final

---
+++ src/component/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Home() {
+    return (
+        <div className='hero' >
+            <div class="card bg-dark text-white">
+                <img src="assets/bg1.jpg.jpg" class="card-img" alt="..."/>
+                    <div class="card-img-overlay">
+                        <h5 class="card-title">Card title</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text">Last updated 3 mins ago</p>
+                    </div>
+            </div>
+        </div>
+    )
+}
+
+export default Home
