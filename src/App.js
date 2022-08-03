a7655a7560967857463fad55c3fabd8913588749
Login-Registration-form-python <vishalkunnam124@gmail.com>
2022-08-03 12:16:09 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import Home from './component/Home'
 import Navbar from './component/Navbar'
+import { Routes, Route } from 'react-router-dom'
+import Products from './component/Products'
 
 function App() {
   return (
     <div>
-      <Navbar/>
-      <Home/>
+
+      <Navbar />
+      <Routes>
+        <Route exact path='/' element={<Home/>} />
+        <Route exact path='/products' element={<Products/>} />
+
+      </Routes>
     </div>
   )
 }
--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
+
 
 function Navbar() {
     return (
@@ -6,34 +8,34 @@ function Navbar() {
             <nav className="navbar navbar-expand-lg navbar-light bg-white py-3 
             shadow-sm ">
                 <div className="container">
-                    <a className="navbar-brand fw-bold fs-4 " href="https://">
+                    <NavLink className="navbar-brand fw-bold fs-4 " to="/">
                         I AM THERE
-                    </a>
+                    </NavLink>
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
                     <div className="collapse navbar-collapse" id="navbarSupportedContent">
                         <ul className="navbar-nav mx-auto mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="https://">Home</a>
+                                <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="https://">Products</a>
+                                <NavLink className="nav-link" to="/products">Products</NavLink>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="https://">About</a>
+                                <NavLink className="nav-link" to="/abouts">About</NavLink>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="https://">Contact</a>
+                                <NavLink className="nav-link" to="/contacts">Contact</NavLink>
                             </li>
                         </ul>
                         <div className="buttons">
-                            <a href="https://" className="btn btn-outline-dark"> 
-                           <i className="fa fa-sign-in me-1"></i> Login</a>
-                            <a href="https://" className="btn btn-outline-dark ms-2"> 
-                           <i className="fa fa-user-plus me-1"></i> Register</a>
-                            <a href="https://" className="btn btn-outline-dark ms-2"> 
-                           <i className="fa fa-shopping-cart me-1"></i> Cart(0)</a>
+                            <NavLink to="https://" className="btn btn-outline-dark"> 
+                           <i className="fa fa-sign-in me-1"></i> Login</NavLink>
+                            <NavLink to="https://" className="btn btn-outline-dark ms-2"> 
+                           <i className="fa fa-user-plus me-1"></i> Register</NavLink>
+                            <NavLink to="https://" className="btn btn-outline-dark ms-2"> 
+                           <i className="fa fa-shopping-cart me-1"></i> Cart(0)</NavLink>
                         </div>
                     </div>
                 </div>
