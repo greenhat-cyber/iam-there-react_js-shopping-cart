e11af9ff0c983bd43077bfd626274497e8133c08
Vishal <66051441+greenhat-cyber@users.noreply.github.com>
2022-08-28 07:17:37 -0400

    update

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -4,18 +4,29 @@ import Products from './Products'
 function Home() {
     return (
         <div className='hero' >
-            <div className="card bg-dark text-white border-0">
-                <img src="assets/bg1.jpg" className="card-img" alt="..."
-                    height='550px' />
-                <div className="card-img-overlay d-flex flex-column justify-content-center">
-                    <div className="container text-dark">
 
-                        <h5 className="card-title display-3 fw-bolder mb-0 ">NEW SEASON ARRIVALS</h5>
-                        <p className ="card-text lead fs-2">CHECK OUT ALL THE TRENDS</p>
+            <div id="carouselExampleFade" className="carousel slide carousel-fade" data-bs-ride="carousel">
+                <div className="carousel-inner">
+                    <div className="carousel-item active" data-bs-interval="2000">
+                        <img src="/assets/slider-4.jpg" className="d-block w-100" alt="..." />
+                    </div>
+                    <div className="carousel-item" data-bs-interval="2000">
+                        <img src="/assets/slider-5.jpg" className="d-block w-100" alt="..." />
+                    </div>
+                    <div className="carousel-item" data-bs-interval="2000">
+                        <img src="/assets/slider-6.jpg" className="d-block w-100" alt="..." />
                     </div>
                 </div>
+                <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+                    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+                    <span className="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
             </div>
-            <Products/>
+            <Products />
         </div>
     )
 }
